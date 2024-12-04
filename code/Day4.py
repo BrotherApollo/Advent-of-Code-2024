3a3d2860b32f7a8782129dbd0a417cd7d1adffef
Caleb <32374545+BrotherApollo@users.noreply.github.com>
2024-12-04 17:59:38 -0500

    improved the array buffer

--- code/Day4.py
+++ code/Day4.py
@@ -2,21 +2,17 @@ import numpy as np
 
 from utils import read_file
 
-test= """
-XMAS
-SAMS
-SAAA
-SMMM
-XXXX
-""".split("\n")
-
 # Building the wordsearch, witha buffer for preventing indexing issues
-test = [list(f"....{x}....") for x in test if x]
-data = [list(f"...{x}....") for x in read_file("input/day4.txt")]
-buffer = [['.' for x in range(len(data[0]))]for x in range(4)]
-word_search = np.array(buffer+data+buffer)
-# print(word_search)    
+def buffer(matrix, dex=3):
+    dex = dex * 2
+    y,x = matrix.shape
+    big_array = np.array([['.' for x in range(x+dex)] for y in range(y+dex)])
+    dex = dex // 2
+    big_array[dex:-dex, dex:-dex] = matrix
+    return big_array
 
+
+# Part 1
 # a function to grab all the possible correct answers for each postion
 def look_around(word_search, row, col):
     grid = word_search
@@ -35,6 +31,8 @@ def look_around(word_search, row, col):
     ]
     return matches
 
+
+# Part 2
 def check_x(word_search, row, col):
     grid = word_search
     matches = [
@@ -46,6 +44,10 @@ def check_x(word_search, row, col):
     ]
     return matches
 
+#Da Loop
+word_search = np.array([list(x) for x in read_file("input/day4.txt")])
+word_search = buffer(word_search)
+print(word_search)
 xmasCount = 0
 x_masCount = 0
 for row in range(len(word_search)):
